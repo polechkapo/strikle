2a7a6fda4363b03acf254a8bec84a29f87d9d7c1
Marie E <marrrielle.e@gmail.com>
2022-08-22 19:44:26 +0300

    created css files for logReg, main, nav

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './features/store';
 import App from './features/App/App';
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
