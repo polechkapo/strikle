5fc57055bd4c88e460a8f83334f376ef428b0ccf
Marie E <marrrielle.e@gmail.com>
2022-08-23 14:25:22 +0300

    fixed css files

--- frontend/src/features/Nav/Nav.jsx
+++ frontend/src/features/Nav/Nav.jsx
@@ -16,19 +16,17 @@ function Nav() {
   };
 
   return (
-    <div className="divNav">
-      {user.user && <button className="btnNav" type="button" onClick={handleDestroy}>Выйти</button> }
-    </div>
     <>
-      {user.user && (
-      <>
-        <button type="button" onClick={handleDestroy}>Выйти</button>
-        <Link to="/tinder">Tinder</Link>
-        <Link to="/">Home</Link>
-        <Link to="/cabinet">Cab</Link>
-      </>
-      )}
-
+      <div className="divNav">
+        {user.user && (
+        <>
+          <button className="btnNav" type="button" onClick={handleDestroy}>Выйти</button>
+          <Link to="/tinder">Tinder</Link>
+          <Link to="/">Home</Link>
+          <Link to="/cabinet">Cab</Link>
+        </>
+        )}
+      </div>
     </>
   );
 }
