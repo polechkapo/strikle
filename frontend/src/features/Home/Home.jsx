7aa4ad2f2b4fbcf7813d92f0090ac977dbe57a7e
Polechka <polechkapo@yandex.ru>
2022-08-24 09:57:58 +0300

    правки верстки

--- frontend/src/features/Home/Home.jsx
+++ frontend/src/features/Home/Home.jsx
@@ -5,7 +5,18 @@ import './Home.css';
 function Home() {
   const navigate = useNavigate();
   return (
-    <div id="bodyReg1" className="logContainer">
+    <>
+      <div id="bodyReg1" className="logContainer">
+        <div className="divLog" />
+        <div className="imgLog1" />
+        <div className="imgLog2" />
+        <div className="imgLog3" />
+        <div className="imgLog4" />
+        <div className="imgLog5" />
+        <div className="imgLog6" />
+        <div className="imgLog7" />
+        <div className="imgLog8" />
+      </div>
       <div className="home__wrapper">
         <h1 className="h1Reg2 home__title">Дополни аккаунт</h1>
         <div className="genres__content home__content">
@@ -17,16 +28,8 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="divLog" />
-      <div className="imgLog1" />
-      <div className="imgLog2" />
-      <div className="imgLog3" />
-      <div className="imgLog4" />
-      <div className="imgLog5" />
-      <div className="imgLog6" />
-      <div className="imgLog7" />
-      <div className="imgLog8" />
-    </div>
+
+    </>
   );
 }
 
