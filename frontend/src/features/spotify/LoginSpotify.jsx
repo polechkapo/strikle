5986f72ee66688b69fd2e53a8d2a699ed5001c00
Vitaliy <vetal9305@gmail.com>
2022-09-04 15:39:31 +0300

    новый билд для херокку

--- frontend/src/features/spotify/LoginSpotify.jsx
+++ frontend/src/features/spotify/LoginSpotify.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import React from 'react';
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a64cff7f1f62470ab9902e118ab37e41&response_type=code&redirect_uri=https://strikle1.herokuapp.com/search';
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=b931afb81c784c88b86e59762b471baa&response_type=code&redirect_uri=https://strikle1.herokuapp.com/search';
 
 export default function LoginSpotify() {
   return (
